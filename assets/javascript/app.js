f43e20ea0da688936056e603f5937cabd33a3489
olivas1406 <olivas1406@msn.com>
2018-03-14 22:48:30 -0700

    HTML & CSS done, working on JS/firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+
+/*     
+     '( 
+    "' //}
+   ( ''"
+   _||__ ____ ____ ____
+  (o)___)}___}}___}}___}
+  'U'0 0  0 0  0 0  0 0  
+*/
+
+
+$(document).ready(function(){ 
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBv2sCzhibbjk_F5ug6VkH-r9V9Y5n8s-M",
+    authDomain: "choochoo-b7192.firebaseapp.com",
+    databaseURL: "https://choochoo-b7192.firebaseio.com",
+    projectId: "choochoo-b7192",
+    storageBucket: "",
+    messagingSenderId: "517666581796"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
